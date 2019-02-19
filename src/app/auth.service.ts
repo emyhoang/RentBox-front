fde797735f661896955d223b94e2b116b83bece6
emyhoang <emyhoang8899@gmail.com>
2019-02-18 22:54:59 -0800

    temp

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,7 +10,7 @@ import { environment } from '../environments/environment'
   providedIn: 'root'
 })
 export class AuthService {
-  private BASE_URL = process.env.BASE_URL || environment.baseUrl
+  private BASE_URL = 'https://intense-dawn-91436.herokuapp.com'
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
 
